70e4ff7c02e4d7b9262502ec164f2b2189ebd9af
Farad2020 <fnirc@mail.ru>
2019-12-18 20:47:00 +0600

    py files

--- py_session/session_check_1.py
+++ py_session/session_check_1.py
@@ -2,16 +2,16 @@ import sqlite3
 
 
 class Car:
-    def __init__(self, car_id, name, driver_licence):
+    def __init__(self, car_id, car_name, driver_licence):
         self.car_id = car_id
-        self.name = name
+        self.car_name = car_name
         self.driver_licence = driver_licence
 
     def getCarName(self):
-        return self.name
+        return self.car_name
 
-    def setCarName(self, name):
-        self.name = name
+    def setCarName(self, car_name):
+        self.car_name = car_name
 
 
 class Driver:
@@ -41,7 +41,7 @@ class Fine:
 
 
 human = Driver("125512", "Mister", "Fastest")
-vehicle = Car(1, "miysuru", human.getLicence)
+vehicle = Car(1, "miysuru", human.getLicence())
 new_fine = Fine(1, vehicle.car_id, "pretty brutal demon killing")
 
 conn = sqlite3.connect("Endterm.db")
@@ -51,9 +51,18 @@ cur.execute("create table if not exists Drivers(driver_licence TEXT, f_name TEXT
 cur.execute("create table if not exists Cars(car_id INTEGER, car_name INTEGER, driver_licence TEXT)")
 cur.execute("create table if not exists Fines(fine_id INTEGER, car_id INTEGER, fine_desc TEXT)")
 
-cur.execute("INSERT INTO Drivers VALUES('%s', '%s', '%s')" %(human.driver_licence, human.f_name, human.s_name))
-conn.commit()
-conn.close()
+cur.execute("INSERT INTO Drivers VALUES('%s', '%s', '%s')" % (human.driver_licence, human.f_name, human.s_name))
+cur.execute("INSERT INTO Cars VALUES('%d', '%s', '%s')" % (vehicle.car_id, vehicle.car_name, vehicle.driver_licence))
+cur.execute("INSERT INTO Fines VALUES('%d', '%d', '%s')" % (new_fine.fine_id, new_fine.car_id, new_fine.fine_desc))
 
+conn.commit()
+
+dvs = cur.execute("select * from Drivers").fetchall()
+crs = cur.execute("select * from Cars").fetchall()
+fns = cur.execute("select * from Fines").fetchall()
+print(dvs)
+print(crs)
+print(fns)
 print('done')
+conn.close()
 
